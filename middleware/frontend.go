f0bd77d5714420fc81f3abdb30ac63de1de92839
zstcode <17093158+zstcode@users.noreply.github.com>
2021-10-08 09:11:40 +0800

    frontend middleware

--- middleware/frontend.go
+++ middleware/frontend.go
@@ -5,16 +5,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/fs"
 	"net/http"
-	"os"
-	"path"
 	"strings"
 )
 
 func exist(fSys fs.FS, path string) bool {
 	f, err := fs.Stat(fSys, path)
-	if os.IsNotExist(err) {
+	if err != nil {
 		return false
 	}
+	//Skip directory
 	if f.IsDir() {
 		return false
 	}
@@ -38,28 +37,16 @@ func FrontendFileHandler(staticFS embed.FS, root string) gin.HandlerFunc {
 			c.Abort()
 			return
 		}
-		//Prevent directory listing
-		if strings.HasSuffix(p, "/") {
-			p = path.Join(p, "index.html")
-		}
-		//Filter the first slash
-		p = p[1:]
 		//Rewrite 404 page to index.html
-		if !exist(fSys, p) {
-			//Do not use c.FileFromFS here, or will cause infinite redirection in index.html<=>./
-			index, err := staticFS.ReadFile(path.Join(root, "index.html"))
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"success": 1})
-				c.Abort()
-				return
-			} else {
-				c.Header("Content-Type", "text/html")
-				c.String(http.StatusOK, string(index))
-				c.Abort()
-				return
-			}
+		//If it is a directory, also return index.html
+		if p == "/" || strings.HasSuffix(p, "/") || !exist(fSys, strings.TrimPrefix(p, "/")) {
+			c.FileFromFS("/", http.FS(fSys))
+		} else if strings.HasSuffix(p, "/index.html") {
+			//Trim the index.html at the end to prevent redirection in ServeHTTP
+			c.FileFromFS(strings.TrimSuffix(p, "index.html"), http.FS(fSys))
+		} else {
+			fileServer.ServeHTTP(c.Writer, c.Request)
 		}
-		fileServer.ServeHTTP(c.Writer, c.Request)
 		c.Abort()
 		return
 
